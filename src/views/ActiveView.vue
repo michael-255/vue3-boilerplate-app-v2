d390f0c2c5f6f4e2202748fd9f4975d889c5f3bf
michaeljoy255 <kodiakchaos@gmail.com>
2022-11-06 18:31:50 -0500

    temp ui for activity

--- src/views/ActiveView.vue
+++ src/views/ActiveView.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { QCard, QCardSection } from 'quasar'
+import { Icon } from '@/constants/ui/icon-enums'
+</script>
 
 <template>
-  <div></div>
+  <!-- Temporary as part fo the Boilerplate App -->
+  <QPage padding>
+    <div class="row justify-center">
+      <div class="col-12">
+        <QCard>
+          <QCardSection class="bg-dark text-white q-py-sm text-h5">
+            <QIcon class="q-pb-xs q-pr-sm" :name="Icon.ACTIVITIES" />
+            Activity Tracker
+          </QCardSection>
+        </QCard>
+      </div>
+    </div>
+  </QPage>
 </template>
